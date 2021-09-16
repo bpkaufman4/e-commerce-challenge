0e77c051b316df1a3fcb1e8d3ca24d4e92a02aff
Brian Kaufman <bpkaufman4@gmail.com>
2021-09-16 10:15:22 -0500

    I cloned the wrong starter code :(

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
-
-// import sequelize constructor
-const Sequelize = require('sequelize');
-
-// import .env
 require('dotenv').config();
 
-// make db connection with MySQL into
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,77 +1,16 @@
-const { animals } = require('./data/animals.json')
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const routes = require('./routes');
+// import sequelize connection
+
 const app = express();
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
+const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(routes);
 
-function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
-    let filteredResults = animalsArray;
-    if (query.personalityTraits) {
-        if (typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits];
-        } else {
-            personalityTraitsArray = query.personalityTraits;
-        }
-        personalityTraitsArray.forEach(trait => {
-            filteredResults = filteredResults.filter(
-                animal => animal.personalityTraits.indexOf(trait) !== -1
-            );
-        });
-    }
-    if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    if (query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    return filteredResults;
-};
-
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-    return result;
-};
-
-function createNewAnimal(body, animalsArray) {
-    console.log(body);
-    // our function's main code will go here
-
-    // return finished code to post route for response
-    return body
-}
-
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-    res.json(result);
-    } else {
-        res.send(404);
-    }
-});
-
-app.post('/api/animals', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
-    
-    res.json(req.body);
-});
-
+// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
+  console.log(`App listening on port ${PORT}!`);
 });
